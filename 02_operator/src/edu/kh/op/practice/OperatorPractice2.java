29cf599dfaec364eebd5d13620aa0b5ffc90e1aa
rlagpwls9795 <rlagpwls9795@naver.com>
2022-08-08 15:45:38 +0900

    Update OperatorPractice2.java

--- 02_operator/src/edu/kh/op/practice/OperatorPractice2.java
+++ 02_operator/src/edu/kh/op/practice/OperatorPractice2.java
@@ -13,12 +13,17 @@ public class OperatorPractice2 {
 		int b = sc.nextInt();
 		System.out.print("번호(정수) : ");
 		int c = sc.nextInt();
+		sc.nextLine();
 		System.out.print("성별(남학생/여학생) : ");
-		String gender = sc.next();
+		String gender = sc.nextLine();
 		System.out.print("성적(소수점 아래 둘째 자리까지) : ");
 		double d = sc.nextDouble();
 		
 		System.out.printf("%d학년 %d반 %d번 %s %s의 성적은 %.2f점 입니다.",a,b,c,name,gender,d);
+		
+		//nextLine()은 신중히 사용. 버퍼 맨 앞에 \n가 남아있을 시 더 이상 입력받지 못하고 넘어간다. 
+		//\n 이 남는 경우 : next(), nextInt(), nextDouble() 등 (nextLine() 제외 거의 모두)
+		//==> 사이에 sc.nextLine(); 작성하면 해결. (입력 버퍼 청소하기)
 		
 	}
 
