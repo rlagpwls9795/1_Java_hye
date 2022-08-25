f2fe040b889d4568d4fc8c41f19f06cd05dc42b3
rlagpwls9795 <rlagpwls9795@naver.com>
2022-08-25 17:05:23 +0900

    20220825 컬렉션

---
+++ 10_collection/src/edu/kh/collection/model/vo/Student.java
@@ -0,0 +1,112 @@
+package edu.kh.collection.model.vo;
+
+import java.util.Objects;
+
+public class Student {
+	private String name;
+	private int age;
+	private String address;
+	private char gender;
+	private int score;
+	
+	public Student() { }
+	
+	public Student(String name, int age, String address, char gender, int score) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.address = address;
+		this.gender = gender;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	//Object.toString() 메서드 오버라이딩
+	//alt+shift+s -> s
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", address=" + address + ", gender=" + gender + ", score="
+				+ score + "]";
+	}
+
+
+	//alt+shift+s -> Override/Implement Method
+//	@Override
+//	public boolean equals(Object obj) { //동등 비교
+//		//매개변수 Object obj == 비교할 객체
+//		if(obj==null) {
+//			return false;
+//		}
+//		if(!(obj instanceof Student)) { //obj가 Student 타입이 아니라면
+//			return false;
+//		} 
+//		//name, age, address, gender, score
+//		Student other=(Student)obj; //다운캐스팅
+//		return this.name.equals(other.name) &&
+//				this.age == other.age &&
+//				this.address.equals(other.address) &&
+//				this.gender==other.gender &&
+//				this.score==other.score;
+//		//모두 같으면 true, 하나라도 다르면 false 반환
+		
+	
+	//hashCode() 오버라이딩 
+	//-> Hash라는 단어가 들어가는 컬렉션 사용 시 반드시 오버라이딩 해야되는 메서드(필드 값을 이용해서 정수를 만들어냄)
+	@Override
+	public int hashCode() {
+		return Objects.hash(address, age, gender, name, score);
+	} //필드가 모두 동일하면 같은 숫자 반환
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(address, other.address) && age == other.age && gender == other.gender
+				&& Objects.equals(name, other.name) && score == other.score;
+	}
+		
+		
+	
+	
+}
+	
+	
+	
+	
+	
+
